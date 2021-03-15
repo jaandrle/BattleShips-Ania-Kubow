b35639114169d618b8d875423921e53a1451a1a9
Jan Andrle <andrle.jan@centrum.cz>
2021-03-15 17:53:47 +0100

    :tada:

--- bin/app.js
+++ bin/app.js
@@ -441,13 +441,11 @@
      * @listens game
      * */
     HTMLBattleShipsElement.prototype.handleEvent= function({ type: event, target, detail: { type, loss, square } }){
-        if(event!=="game") return false;
-        if(type==="round-start") return false;
+        if( event!=="game" || type==="round-start" || type==="start" ) return false;
         const game= _private.get(this);
         let { current_player_id, players= [], results }= game;
         let state= "start";
         switch (type){
-            case "start": return false;
             case "ready":
                 if(game.players.filter(({ name })=> name).length!==2) return setTimeout(dispatchGameEvent, 250, target, { type, loss });
                 current_player_id= game.players.findIndex(({ name })=> name===target.getAttribute("player"));
@@ -458,38 +456,47 @@
                 results= players.map(()=> game.types_ships.map(s=> s.length));
                 break;
             case "fire":
-                state= "game";
-                const main_event_data= { type: "round-end", current_player_id, square_id: square };
-                if(!loss){
-                    dispatchGameEvent(this.shadowRoot, main_event_data);
-                    break;
-                }
-    
-                const opponent_player_id= current_player_id ? 0 : 1;
-                const ship_id= game.types_ships.findIndex(s=> s.name===loss);
-                results[opponent_player_id][ship_id]-= 1;
-                players[opponent_player_id].loss+= 1;
-                if(results[opponent_player_id].filter(Boolean).length){
-                    dispatchGameEvent(this.shadowRoot, Object.assign(main_event_data, {
-                        ship_id, remains: results[opponent_player_id][ship_id]
-                    }));
-                    break;
-                }
-                
-                state= "end";
-                dispatchGameEvent(this.shadowRoot, { type: "end", winner: current_player_id });
+                state= onFire(this, current_player_id);
                 break;
-            default :
         }
         current_player_id= current_player_id ? 0 : 1;
         updateGame(game, { state, current_player_id, players, results });
         this.updateMessages(game);
         if(state==="game") return this.nextRound();
-        if(state!=="end") return false;
+        
+        if(state==="end")
+            players.forEach(({ player })=>
+                ( this.shadowRoot.querySelector(".grid-"+player).onclick= null ));
     
-        players.forEach(({ player })=>
-            ( this.shadowRoot.querySelector(".grid-"+player).onclick= null ));
+        /**
+         * @param {HTMLBattleShipsElement} _this
+         * @param {0|1} current_player_id
+         * @returns {"game"|"end"} Zda se ještě hraje
+         */
+        function onFire(_this, current_player_id){
+            const state_default= "game";
+            const main_event_data= { type: "round-end", current_player_id, square_id: square };
+            if(!loss){
+                dispatchGameEvent(_this.shadowRoot, main_event_data);
+                return state_default;
+            }
+    
+            const opponent_player_id= current_player_id ? 0 : 1;
+            const ship_id= game.types_ships.findIndex(s=> s.name===loss);
+            results[opponent_player_id][ship_id]-= 1;
+            players[opponent_player_id].loss+= 1;
+            if(results[opponent_player_id].filter(Boolean).length){
+                dispatchGameEvent(_this.shadowRoot, Object.assign(main_event_data, {
+                    ship_id, remains: results[opponent_player_id][ship_id]
+                }));
+                return state_default;
+            }
+            
+            dispatchGameEvent(_this.shadowRoot, { type: "end", winner: current_player_id });
+            return "end";
+        }
     };
+    
     HTMLBattleShipsElement.prototype.log= function(){
         console.log(_private.get(this)); /* jshint devel: true *///gulp.keep.line
     };
--- src/js/components/g-battleships/handleEvent.sub.js
+++ src/js/components/g-battleships/handleEvent.sub.js
@@ -1,4 +1,4 @@
-/* jshint maxcomplexity: 15 */
+/* jshint maxcomplexity: 12 */
 /* parent *//* global HTMLBattleShipsElement, _private */
 gulp_place("../../game/updateGame.sub.js", "file_once");/* global updateGame */
 gulp_place("../../dispatchGameEvent.sub.js", "file_once");/* global dispatchGameEvent */
@@ -9,13 +9,11 @@ gulp_place("../../GameEventData.type.sub.js", "file_once");
  * @listens game
  * */
 HTMLBattleShipsElement.prototype.handleEvent= function({ type: event, target, detail: { type, loss, square } }){
-    if(event!=="game") return false;
-    if(type==="round-start") return false;
+    if( event!=="game" || type==="round-start" || type==="start" ) return false;
     const game= _private.get(this);
     let { current_player_id, players= [], results }= game;
     let state= "start";
     switch (type){
-        case "start": return false;
         case "ready":
             if(game.players.filter(({ name })=> name).length!==2) return setTimeout(dispatchGameEvent, 250, target, { type, loss });
             current_player_id= game.players.findIndex(({ name })=> name===target.getAttribute("player"));
@@ -26,35 +24,43 @@ HTMLBattleShipsElement.prototype.handleEvent= function({ type: event, target, de
             results= players.map(()=> game.types_ships.map(s=> s.length));
             break;
         case "fire":
-            state= "game";
-            const main_event_data= { type: "round-end", current_player_id, square_id: square };
-            if(!loss){
-                dispatchGameEvent(this.shadowRoot, main_event_data);
-                break;
-            }
-
-            const opponent_player_id= current_player_id ? 0 : 1;
-            const ship_id= game.types_ships.findIndex(s=> s.name===loss);
-            results[opponent_player_id][ship_id]-= 1;
-            players[opponent_player_id].loss+= 1;
-            if(results[opponent_player_id].filter(Boolean).length){
-                dispatchGameEvent(this.shadowRoot, Object.assign(main_event_data, {
-                    ship_id, remains: results[opponent_player_id][ship_id]
-                }));
-                break;
-            }
-            
-            state= "end";
-            dispatchGameEvent(this.shadowRoot, { type: "end", winner: current_player_id });
+            state= onFire(this, current_player_id);
             break;
-        default :
     }
     current_player_id= current_player_id ? 0 : 1;
     updateGame(game, { state, current_player_id, players, results });
     this.updateMessages(game);
     if(state==="game") return this.nextRound();
-    if(state!=="end") return false;
+    
+    if(state==="end")
+        players.forEach(({ player })=>
+            ( this.shadowRoot.querySelector(".grid-"+player).onclick= null ));
 
-    players.forEach(({ player })=>
-        ( this.shadowRoot.querySelector(".grid-"+player).onclick= null ));
+    /**
+     * @param {HTMLBattleShipsElement} _this
+     * @param {0|1} current_player_id
+     * @returns {"game"|"end"} Zda se ještě hraje
+     */
+    function onFire(_this, current_player_id){
+        const state_default= "game";
+        const main_event_data= { type: "round-end", current_player_id, square_id: square };
+        if(!loss){
+            dispatchGameEvent(_this.shadowRoot, main_event_data);
+            return state_default;
+        }
+
+        const opponent_player_id= current_player_id ? 0 : 1;
+        const ship_id= game.types_ships.findIndex(s=> s.name===loss);
+        results[opponent_player_id][ship_id]-= 1;
+        players[opponent_player_id].loss+= 1;
+        if(results[opponent_player_id].filter(Boolean).length){
+            dispatchGameEvent(_this.shadowRoot, Object.assign(main_event_data, {
+                ship_id, remains: results[opponent_player_id][ship_id]
+            }));
+            return state_default;
+        }
+        
+        dispatchGameEvent(_this.shadowRoot, { type: "end", winner: current_player_id });
+        return "end";
+    }
 };
